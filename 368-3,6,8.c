d4f88892e75a962f293791de4d57b6fbbbda1c7f
Rutuja Shelke <129090371+909054rutu@users.noreply.github.com>
2023-12-14 08:25:25 +0530

    Add files via upload

---
+++ 368-3,6,8.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<conio.h>
+#include<malloc.h>
+struct node
+{
+int data;
+struct node *next;
+};
+struct node *start=NULL,*temp=NULL;
+int main()
+{
+int num,a[10],i,j;
+printf("enter the number:-");
+scanf("%d",&num);
+i=0;
+while(num>0)
+{
+a[i]=num%10;
+i++;
+num=num/10;
+}
+i--;
+printf("\nthe display of linked list is:-\n");
+for(j=i;j>=0;j--)
+{
+if(start==NULL)
+{
+start=malloc(sizeof(struct node));
+start->data=a[j];
+printf("%d",start->data);
+start->next=NULL;
+temp=start;
+}
+else
+{
+temp->next=malloc(sizeof(struct node));
+temp->next->data=a[j];
+printf(",%d",temp->next->data);
+temp->next->next=NULL;
+temp=temp->next;
+}
+}
+
+}
